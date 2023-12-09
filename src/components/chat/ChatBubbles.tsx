454b7fc5339e133d7c75b3f480ac0d059d67e821
TaladeogoA <ataladeogo@gmail.com>
2023-12-09 19:23:18 +0100

    Add react-geocode library and implement helper functions for getting coordinates and nearby hospitals

--- src/components/chat/ChatBubbles.tsx
+++ src/components/chat/ChatBubbles.tsx
@@ -1,89 +1,111 @@
-import { Flex, HStack, Box, keyframes } from '@chakra-ui/react'
-import { MedAIChatAvatar, UserChatAvatar } from "./Avatars"
-import { MedAITextContent, UserTextContent } from "./TextContents"
-import { Children } from "@/types/index"
+import { Flex, HStack, Box, keyframes } from "@chakra-ui/react";
+import { MedAIChatAvatar, UserChatAvatar } from "./Avatars";
+import { MedAITextContent, UserTextContent } from "./TextContents";
+import { Children } from "@/types/index";
 
-const Bubble = ({ children, justification, direction, gap }: Children & {
-  justification: "start" | "end"
-  direction?: "row" | "row-reverse",
-  gap: { [x:string]: string }
+const Bubble = ({
+  children,
+  justification,
+  direction,
+  gap
+}: Children & {
+  justification: "start" | "end";
+  direction?: "row" | "row-reverse";
+  gap: { [x: string]: string };
 }) => {
   return (
-    <Flex flexDirection={direction || "row"} justify={justification} w="100%" gap={gap}>
+    <Flex
+      flexDirection={direction || "row"}
+      justify={justification}
+      w="100%"
+      gap={gap}
+    >
       {children}
     </Flex>
-  )
-}
+  );
+};
 
 export const UserBubble = ({
   content,
-  user,
+  user
 }: {
-  content: string
+  content: string;
   user: {
-    profileImage: string,
-    firstName: string,
-    lastName: string
-  }
+    profileImage: string;
+    firstName: string;
+    lastName: string;
+  };
 }) => {
-
   return (
-    <Bubble gap={{"2xs": "10px", md: "24px"}} direction="row-reverse" justification="end">
-      <UserChatAvatar imageSrc={user.profileImage} name={`${user.firstName} ${user.lastName}`} />
-      <UserTextContent content="Hey, I've been experiencing frequent fatigue and difficulty concentrating lately. What could be causing this, and what are some remedies or lifestyle changes I could consider?"/>
+    <Bubble
+      gap={{ "2xs": "10px", md: "24px" }}
+      direction="row-reverse"
+      justification="end"
+    >
+      <UserChatAvatar
+        imageSrc={user.profileImage}
+        name={`${user.firstName} ${user.lastName}`}
+      />
+      <UserTextContent content={content} />
     </Bubble>
-  )
-}
+  );
+};
 
-export const AIBubble = ({
-  content
-}: {
-  content: string
-}) => {
-
+export const AIBubble = ({ content }: { content: string }) => {
   return (
-    <Bubble gap={{"2xs": "10px", md: "16px"}}  direction="row" justification="start">
+    <Bubble
+      gap={{ "2xs": "10px", md: "16px" }}
+      direction="row"
+      justification="start"
+    >
       <MedAIChatAvatar />
-      <MedAITextContent content="Feeling fatigued and having difficulty concentrating can stem from various factors. It might be due to inadequate sleep, excessive stress, poor nutrition, or even an underlying health issue. To provide more tailored guidance, could you share any recent changes in your routine or additional symptoms you've noticed? "/>
+      <MedAITextContent content="Feeling fatigued and having difficulty concentrating can stem from various factors. It might be due to inadequate sleep, excessive stress, poor nutrition, or even an underlying health issue. To provide more tailored guidance, could you share any recent changes in your routine or additional symptoms you've noticed? " />
     </Bubble>
-  )
-}
+  );
+};
 
 export const AIBubbleLoading = () => {
-
   const bounce = keyframes`  
     from {transform: translateY(-10px)}   
     to {transform: translateY(10px)}
   `;
 
   return (
-    <Bubble gap={{"2xs": "10px", md: "16px"}}  direction="row" justification="start">
+    <Bubble
+      gap={{ "2xs": "10px", md: "16px" }}
+      direction="row"
+      justification="start"
+    >
       <MedAIChatAvatar />
       <HStack spacing="2rem" position="relative">
-        <Box  
+        <Box
           animation={`${bounce} infinite 1500ms ease-in-out`}
-          position="absolute" 
-          left="30px" w={{"2xs": "0.9rem", md: "1.5rem"}} 
-          h={{"2xs": "0.9rem", md: "1.5rem"}} 
-          bg='primary.600' 
-          borderRadius="50%"></Box>
-        <Box  
+          position="absolute"
+          left="30px"
+          w={{ "2xs": "0.9rem", md: "1.5rem" }}
+          h={{ "2xs": "0.9rem", md: "1.5rem" }}
+          bg="primary.600"
+          borderRadius="50%"
+        ></Box>
+        <Box
           animation={`${bounce} infinite 1500ms 500ms ease-in-out`}
-          position="absolute" 
-          left="60px" 
-          w={{"2xs": "0.9rem", md: "1.5rem"}}
-          h={{"2xs": "0.9rem", md: "1.5rem"}} 
-          bg='primary.600'
-          borderRadius="50%"></Box>
-        <Box  
+          position="absolute"
+          left="60px"
+          w={{ "2xs": "0.9rem", md: "1.5rem" }}
+          h={{ "2xs": "0.9rem", md: "1.5rem" }}
+          bg="primary.600"
+          borderRadius="50%"
+        ></Box>
+        <Box
           animation={`${bounce} infinite 1500ms 1000ms ease-in-out`}
-          position="absolute" 
-          left="90px" 
-          w={{"2xs": "0.9rem", md: "1.5rem"}} 
-          h={{"2xs": "0.9rem", md: "1.5rem"}} 
-          bg='primary.600' 
-          borderRadius="50%"></Box>
+          position="absolute"
+          left="90px"
+          w={{ "2xs": "0.9rem", md: "1.5rem" }}
+          h={{ "2xs": "0.9rem", md: "1.5rem" }}
+          bg="primary.600"
+          borderRadius="50%"
+        ></Box>
       </HStack>
     </Bubble>
-  )
-}
+  );
+};
