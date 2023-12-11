65a15937b7f0ae6eddcb56f1ce524516a9ad4d4c
exploitenomah <iksploiting@gmail.com>
2023-12-11 13:22:35 +0100

    prevent new chat creation if current chat is a new chat

--- src/components/buttons/CreateNewChatButton.tsx
+++ src/components/buttons/CreateNewChatButton.tsx
@@ -3,21 +3,24 @@ import { Flex } from "@chakra-ui/react"
 import { useCallback } from "react"
 import { Children } from "@/types/index"
 import { createNewConversation, updateLoading } from '@/redux/conversations/slice';
-import { useAppDispatch } from '@/redux/hooks';
+import { useAppDispatch, useAppSelector } from '@/redux/hooks';
 import { useRouter, usePathname } from 'next/navigation';
 
 const CreateNewChatButton = ({ children, onCreateNewChat }: Children & {
   onCreateNewChat?: () => void
 }) => {
+  const { activeConversationId } = useAppSelector(store => store.conversations)
   const router = useRouter();
   const pathname = usePathname()
   const dispatch = useAppDispatch();
+
   const createNewChat = useCallback(() => {
+    if(pathname === "/" && activeConversationId !== null) return 
     dispatch(createNewConversation());
     dispatch(updateLoading(true))
     if (onCreateNewChat) onCreateNewChat();
     pathname !== "/" && router.push('/');
-  }, [onCreateNewChat, dispatch, router, pathname]);
+  }, [onCreateNewChat, dispatch, router, pathname, activeConversationId]);
 
   return (
     <Flex as="button" onClick={createNewChat}>
