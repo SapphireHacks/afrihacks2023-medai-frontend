0bec595d559badda3bee9a4b86b99a8b64cf6c75
exploitenomah <iksploiting@gmail.com>
2023-12-12 05:35:32 +0100

    add required username field to signup form

---
+++ src/components/auth/Layout.tsx
@@ -0,0 +1,41 @@
+import {
+  Box,
+  Flex,
+} from '@chakra-ui/react';
+import Image, { StaticImageData } from 'next/image';
+import { Children } from "@/types";
+
+const Layout = ({ children, imageAlt, imageSource }: Children & {
+  imageSource: StaticImageData | string 
+  imageAlt: string
+}) => {
+ 
+
+  return (
+    <Flex w="100dvw" minH="100dvh" justify="center" align="center">
+      <Flex
+        justifyContent={{md: "space-between"}}
+        align={{md: "center"}}
+        w="90%"
+        mx="auto" 
+        flexDir={{ base: 'column-reverse', md: 'row' }}>
+        <Box textAlign={{ base: 'center', md: 'left' }} flexGrow="1" maxW={{md:"38%"}}>
+          {children}
+        </Box>
+        <Box as={Flex} flexGrow="1" maxW={{ md: "60%" }} h={{ base: "30vh", md: "calc(100dvh - 160px)" }}>
+          <Image
+            src={imageSource}
+            alt={imageAlt}
+            style={{
+              width: '100%',
+              height: "100%",
+              objectFit: "contain"
+            }}
+          />
+        </Box>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Layout;
--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -10,7 +10,8 @@ import {
   InputGroup,
   InputRightElement,
   Link,
-  Text
+  Text,
+  VStack
 } from '@chakra-ui/react';
 import Image from 'next/image';
 import LoginImage from '@/assets/images/login-image.png';
@@ -23,6 +24,9 @@ import urls from '../../services/urls';
 import { useRouter } from 'next/router';
 import { useAppDispatch } from '@/redux/hooks';
 import { setUser } from '@/redux/user/slice';
+import { BasicInput, PasswordTypeInput, SubmitButton } from "@/components/auth/Inputs";
+import { FormHeading, Paragraph } from "@/components/auth/Text";
+import Layout from "@/components/auth/Layout";
 
 const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -34,7 +38,7 @@ const Login = () => {
 
   const formHook = useForm({
     defaultValues: {
-      email: '',
+      emailOrUserName: '',
       password: ''
     }
   });
@@ -46,7 +50,7 @@ const Login = () => {
   } = formHook;
 
   const submit: SubmitHandler<{
-    email: string;
+    emailOrUserName: string;
     password: string;
   }> = async (data: any) => {
     if (!data) return;
@@ -77,167 +81,72 @@ const Login = () => {
   };
 
   return (
-    <Flex
-      pt={{
-        base: '3rem',
-        md: '6rem'
-      }}
-      justifyContent="space-between"
-      flexDir={{
-        base: 'column-reverse',
-        md: 'row'
-      }}
-      px={{
-        base: '0',
-        md: '8rem'
-      }}
-    >
-      <Box
-        w={{
-          base: '90%',
-          md: '50%'
-        }}
-        mx={{
-          base: '2rem',
-          md: 'unset'
-        }}
-        mt="4rem"
-      >
-        <Box
-          w={{
-            base: '100%',
-            md: '80%'
-          }}
-          textAlign={{
-            base: 'center',
-            md: 'unset'
-          }}
-          mb="4rem"
-        >
-          <Text fontSize="3xl" fontWeight="550">
-            Welcome Back!
-          </Text>
-          <Text
-            fontSize={{
-              base: 'base',
-              md: 'xl'
-            }}
-            fontWeight="400"
-          >
-            Login to your account to continue
-          </Text>
+    <Layout imageSource={LoginImage} imageAlt={"Login Illustration"}>
+      <Box w="100%">
+        <FormHeading >
+          Welcome Back!
+        </FormHeading>
+        <Paragraph>
+          Login to your account to continue
+        </Paragraph>
 
-          <form onSubmit={handleSubmit(submit)}>
-            <Flex flexDir="column" mt="2rem" gap="1.5rem">
-              <FormControl isRequired>
-                <FormLabel fontSize="lg">Email Address</FormLabel>
-                <Input
-                  type="email"
-                  h="5rem"
-                  borderRadius="0.5rem"
-                  fontSize="base"
-                  placeholder="Enter your email address"
-                  {...register('email', {
-                    required: 'Email is required',
-                    pattern: {
-                      value: /\S+@\S+\.\S+/,
-                      message: 'Entered value does not match email format'
-                    }
-                  })}
-                />
-              </FormControl>
-              <FormControl isRequired>
-                <FormLabel fontSize="lg">Password</FormLabel>
-                <InputGroup size="md">
-                  <Input
-                    pr="4.5rem"
-                    type={showPassword ? 'text' : 'password'}
-                    h="5rem"
-                    borderRadius="0.5rem"
-                    fontSize="base"
-                    placeholder="Enter your password"
-                    {...register('password', {
-                      required: 'Password is required'
-                    })}
-                  />
-                  <InputRightElement width="4.5rem" mt="1.25rem">
-                    <IconButton
-                      bg="transparent"
-                      h="1.5rem"
-                      aria-label="hide password"
-                      icon={<EyeOffIcon />}
-                      onClick={handleClick}
-                      _hover={{ bg: 'transparent' }}
-                    />
-                  </InputRightElement>
-                </InputGroup>
-              </FormControl>
-              <Flex justifyContent="space-between" alignItems="center">
-                <Checkbox
-                  iconSize="2rem"
-                  onChange={() => setRememberMe(!rememberMe)}
-                  isChecked={rememberMe}
-                >
-                  <Text fontSize="sm">Remember me</Text>
-                </Checkbox>
+        <VStack as="form" spacing="2.4rem" w="100%" onSubmit={handleSubmit(submit)}>
+          <Flex flexDir="column" mt="2rem" gap="1.5rem" w="100%">
+            <BasicInput
+              required
+              labelText="Email Address/Username"
+              placeholder="Enter your preferred username"
+              type="text"
+              {...register('emailOrUserName', {
+                required: 'This field is required',
+              })}
+            />
 
-                <Link
-                  href="/auth/forgot-password"
-                  fontSize="sm"
-                  color="primary.900"
-                  _hover={{ textDecoration: 'none' }}
-                >
-                  Forgot Password?
-                </Link>
-              </Flex>
-              <Button
-                type="submit"
-                h="4rem"
-                borderRadius="0.5rem"
-                fontSize="lg"
-                fontWeight="400"
-                bg="primary.900"
-                mt="2rem"
-                color="white"
-                _hover={{ bg: 'primary.700' }}
-                isLoading={loading}
+            <PasswordTypeInput
+              required
+              labelText="Password"
+              placeholder="Enter your password"
+              type="password"
+              {...register('password', {
+                required: 'Password is required',
+              })}
+            />
+            <Flex justifyContent="space-between" alignItems="center">
+              <Checkbox
+                iconSize="2rem"
+                onChange={() => setRememberMe(!rememberMe)}
+                isChecked={rememberMe}
               >
-                Login
-              </Button>
-            </Flex>
-          </form>
-          <Text textAlign="center" fontSize="lg" fontWeight="400" mt="2rem">
-            I have don&apos;t an account?{' '}
-            <Link href="/auth/signup">
-              <Text
-                as="span"
+                <Text fontSize="sm">Remember me</Text>
+              </Checkbox>
+
+              <Link
+                href="/auth/forgot-password"
+                fontSize="sm"
                 color="primary.900"
-                fontWeight="600"
-                cursor="pointer"
+                _hover={{ textDecoration: 'none' }}
               >
-                Sign up
-              </Text>
-            </Link>
-          </Text>
-        </Box>
+                Forgot Password?
+              </Link>
+            </Flex>
+            <SubmitButton>Login</SubmitButton>
+          </Flex>
+        </VStack>
+        <Text textAlign="center" fontSize="lg" fontWeight="400" mt="2rem">
+          I have don&apos;t an account?{' '}
+          <Link href="/auth/signup">
+            <Text
+              as="span"
+              color="primary.900"
+              fontWeight="600"
+              cursor="pointer"
+            >
+              Sign up
+            </Text>
+          </Link>
+        </Text>
       </Box>
-      <Box
-        w={{
-          base: '80%',
-          md: '45%'
-        }}
-        mx="auto"
-      >
-        <Image
-          src={LoginImage}
-          alt="login illustration"
-          style={{
-            width: '100%',
-            height: '100%'
-          }}
-        />
-      </Box>
-    </Flex>
+    </Layout >
   );
 };
 
