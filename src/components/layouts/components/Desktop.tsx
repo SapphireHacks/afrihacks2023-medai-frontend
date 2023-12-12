5ffe119ed951400f90705fa67ea231d41fdd1b47
Taladeogo Abraham <ataladeogo@gmail.com>
2023-12-12 08:31:19 +0100

    Update DesktopLayout component in Desktop.tsx

--- src/components/layouts/components/Desktop.tsx
+++ src/components/layouts/components/Desktop.tsx
@@ -4,11 +4,18 @@ import Navigation from './Navigation';
 
 const DesktopLayout = ({ children }: Children) => {
   return (
-    <Grid templateColumns={'repeat(12, 1fr)'} gap={'1rem'} mx="auto" h="100vh">
-      <GridItem colSpan={3} bg="primary.50">
+    <Grid
+      templateColumns={'repeat(12, 1fr)'}
+      gap={'1rem'}
+      mx="auto"
+      h="100vh"
+      maxH="100vh"
+      overflow="hidden"
+    >
+      <GridItem colSpan={3} bg="primary.50" maxH="100vh">
         <Navigation />
       </GridItem>
-      <GridItem colSpan={9} position="relative">
+      <GridItem colSpan={9} position="relative" maxH="100vh">
         {children}
       </GridItem>
     </Grid>
